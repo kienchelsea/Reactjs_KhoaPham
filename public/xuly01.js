057b6e8e2f0a024671e935923d3bce242d34d7e1
Lê Kiên <ltk182001@gmail.com>
2021-11-03 22:26:14 +0700

    sixthCommit

--- public/xuly01.js
+++ public/xuly01.js
@@ -33,11 +33,51 @@ class TungDuong extends React.Component {
         )
     }
 }
+class Interval extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { hinhmoi: 1 };
+    }
+    changeImage = () => {
+        this.setState({hinhmoi: (this.state.hinhmoi % 5) + 1});
+    }
+    render()  {
+        return(
+            <img class = "anhmoi" src = {"images/" + this.state.hinhmoi + ".jpg"} />
+        )
+    }
+    componentDidMount(){
+        setInterval(this.changeImage, 1000);
+    }
+}
+class List extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { array: ["Hello", "Hi", "KienLe"] };
+    }
+    add = () => {
+        this.state.array.push("ReactJS", "NodeJS");
+        this.setState(this.state)
+    }
+    render() {
+        return(
+            <div>
+                <button onClick = {this.add}>Thêm</button>
+                {
+                this.state.array.map(function(everyone, index){
+                    return <h1 key = {index}>{everyone}</h1>
+                })
+                }
+            </div>
+        )
+    }
+}
 
 
 ReactDOM.render(
     <div>
-        <h1>KienLe Traning</h1>
+        <List/>
+        <Interval />
         <KienLe />
         <TungDuong />
     </div>
